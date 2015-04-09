7a79644128f251a04527d973d6a2288534691be3
Will Robinson <will@wambl.com>
2015-04-09 13:47:27 -0700

    INITIAL

---
+++ Golf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Golf
+//
+//  Created by Will Robinson on 4/9/15.
+//  Copyright (c) 2015 Will Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
